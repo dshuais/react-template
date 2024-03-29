c4411a2f8f30370af95f85a4415a55494398181d
dushuais <1137896420@qq.com>
2024-03-29 22:48:34 +0800

    ✨ feat: 使用modulecss

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,14 +2,14 @@
  * @Author: dushuai
  * @Date: 2024-03-29 16:10:20
  * @LastEditors: dushuai
- * @LastEditTime: 2024-03-29 18:34:14
- * @description: 心平气和
+ * @LastEditTime: 2024-03-29 22:33:18
+ * @description: Home
  */
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import reactLogo from '../../assets/react.svg'
 import viteLogo from '/vite.svg'
-import './index.css'
+import styles from './index.module.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,17 +21,17 @@ function App() {
   }
 
   return (
-    <>
+    <div className={styles.root}>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className={styles.logo} alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className={`${styles.logo} ${styles.react}`} alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className={styles.card}>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -43,10 +43,10 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className={styles['read-the-docs']}>
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
