4a08c51e03eceb6e68d6fe89ec7539ce4a6b8f29
dshuais <1137896420@qq.com>
2024-03-29 17:04:20 +0800

    ✨ feat: 新增axios和全局声明文件

---
+++ src/typings/app.d.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author: dushuai
+ * @Date: 2023-03-20 09:33:25
+ * @LastEditors: dushuai
+ * @LastEditTime: 2024-03-29 17:01:08
+ * @description: ts类型文件
+ */
+
+/**
+ * App内数据类型
+ */
+declare namespace App {
+
+}
