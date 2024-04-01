069e766069923e06f1bd3cbf1e06c6508d9cc266
dshuais <1137896420@qq.com>
2024-04-01 10:04:00 +0800

    ✨ feat: 新增utils

--- src/typings/app.d.ts
+++ src/typings/app.d.ts
@@ -2,9 +2,19 @@
  * @Author: dushuai
  * @Date: 2023-03-20 09:33:25
  * @LastEditors: dushuai
- * @LastEditTime: 2024-03-29 17:01:08
+ * @LastEditTime: 2024-04-01 10:02:01
  * @description: ts类型文件
  */
+
+interface Window {
+  mozRequestAnimationFrame: Function,
+
+  webkitRequestAnimationFrame: Function,
+
+  msRequestAnimationFrame: Function,
+
+  mozCancelAnimationFrame: Function
+}
 
 /**
  * App内数据类型
