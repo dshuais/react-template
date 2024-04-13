6cfb2f6778b8460569bc5dec8492ac2915be63f7
dushuais <1137896420@qq.com>
2024-04-13 23:36:46 +0800

    ✨ feat: 优化动态路由

---
+++ src/common/enums.ts
@@ -0,0 +1,17 @@
+/*
+ * @Author: dushuai
+ * @Date: 2024-04-13 23:20:11
+ * @LastEditors: dushuai
+ * @LastEditTime: 2024-04-13 23:28:29
+ * @Description: 枚举文件
+ */
+
+/**
+ * 路由
+ */
+export enum Pages {
+  Home = '/',
+  Login = '/login',
+  Error = '/error',
+  Home2 = '/home2'
+}
