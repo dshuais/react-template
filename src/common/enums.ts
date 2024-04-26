89c4827c9a9a140f9f3b90f33beb786c6243bc60
dshuais <1137896420@qq.com>
2024-04-26 12:07:07 +0800

    ✨ feat: 迁移zustand

--- src/common/enums.ts
+++ src/common/enums.ts
@@ -2,9 +2,11 @@
  * @Author: dushuai
  * @Date: 2024-04-13 23:20:11
  * @LastEditors: dushuai
- * @LastEditTime: 2024-04-13 23:28:29
+ * @LastEditTime: 2024-04-26 12:05:11
  * @Description: 枚举文件
  */
+
+export const APP_KEY = 'react-template'
 
 /**
  * 路由
@@ -14,4 +16,12 @@ export enum Pages {
   Login = '/login',
   Error = '/error',
   Home2 = '/home2'
+}
+
+/**
+ * store key
+ */
+export enum StoreKey {
+  APP = `app-store-${APP_KEY}`,
+  SETTINGS = `settings-store-${APP_KEY}`
 }
--- src/common/index.ts
+++ src/common/index.ts
@@ -1,2 +1,10 @@
+/*
+ * @Author: dushuai
+ * @Date: 2024-04-11 11:09:55
+ * @LastEditors: dushuai
+ * @LastEditTime: 2024-04-26 12:05:21
+ * @description: 公共配置
+ */
 export * from './context'
 export * from './routes'
+export * from './enums'
